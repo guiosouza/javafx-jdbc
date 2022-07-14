d323ea3e44f8081baaf650a9042d1439fb2437c9
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-07-14 10:06:18 -0300

    Adicionado DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,54 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+
+	// Declaração dos componentes da tela
+	
+	
+	@FXML
+	private TextField txtId; // atributo captado na tela
+	
+	@FXML
+	private TextField txtName; // atributo captado na tela
+	
+	@FXML
+	private Label labelErrorName; // captando para mostrar erro no label
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	private void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	private void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+}
